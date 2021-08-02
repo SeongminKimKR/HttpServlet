b051e76e395ae09e0afaa2b6be786064d38e12b3
unknown <gvf156@gmail.com>
2021-08-02 16:15:13 +0900

    Initial Commit

---
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -0,0 +1,18 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * 하위 패키지의 서블릿을 스캐하여 자동등록
+ */
+@ServletComponentScan
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+	}
+
+}
