4f6f5a724ad5972c5427e38424b0735e871b294d
unknown <gvf156@gmail.com>
2021-08-04 17:45:35 +0900

    [FEAT] Create user controller by spring mvc

--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -6,6 +6,10 @@ import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 @Component ("/springmvc/old-controller")
 public class OldController implements Controller {
@@ -13,5 +17,6 @@ public class OldController implements Controller {
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
         return new ModelAndView("new-form");
+
     }
 }
